4ff49ba2eff15eba31334fef546871eecc26d08a
Anna Moritz <72981027+Mac242@users.noreply.github.com>
2021-12-09 12:19:37 +0100

    enabele / disable collider

--- Assets/Scripts/WaveScript.cs
+++ Assets/Scripts/WaveScript.cs
@@ -7,33 +7,25 @@ using UnityEngine.SocialPlatforms;
 
 public class WaveScript : MonoBehaviour
 {
-
     //Hintergrund f�hrt von rechts nach links durch das Bild, Player l�uft auf der Stelle
     //Random Geist spawnd und f�hrt von rechts nach links durch das Bild
     //Geist reagiert auf das Winken und T�r �ffnet sich - Level geschafft
-
-
     // Arme initialisieren
     public GameObject NormalArm;
     public GameObject WaveArm;
 	public GameObject player;
-    public float speed;
-    public bool waving;
-    
-    
+    public float speed; // speed for Player
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 5f;
-        waving = false;
-        
+        GetComponent<BoxCollider>().enabled = false; // disables the BoxCollider 
     }
 
     // Update is called once per frame
     void Update()
-    {
-
+    { 
         player.transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World); //move Forward
 
         //Normaler Arm wird ausgeblendet, solange Leertaste gedr�ckt ist. 
@@ -43,16 +35,15 @@ public class WaveScript : MonoBehaviour
         {
             NormalArm.SetActive(false);
             WaveArm.SetActive(true);
-            waving = true;
-        };
+            GetComponent<BoxCollider>().enabled = true; // enables the Box Collider, scoring is possible
+
+        }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
             NormalArm.SetActive(true);
             WaveArm.SetActive(false);
-            waving = false;
+            GetComponent<BoxCollider>().enabled = false; // disables the BoxCollider, no scoring possible
         }
     }
-
-    
 }
